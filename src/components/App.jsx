28d0f0f93de373c1dd4d4b64d211f69f94e6bf88
yurii2007 <yurash862@gmail.com>
2023-08-28 17:53:02 +0300

    fix loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,54 +8,59 @@ import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 
 export const App = () => {
-  const [query,setQuery] = useState('');
+  const [query, setQuery] = useState('');
   const [page, setPage] = useState(1);
   const [images, setImages] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState('');
   const [isModalShow, setModalShow] = useState(false);
   const [modalImg, setModalImg] = useState(null);
   const [isLoadMore, setIsLoadMore] = useState(false);
 
-  useEffect(()=>{
-    if(!query) return
-    setIsLoading(true);
-    getImages(query,page).then(({totalHits,hits})=>{
-      setImages(prevImgs=>[...prevImgs,...hits])
-      setIsLoadMore(totalHits > 0 && page < Math.ceil(totalHits / 12))
-    }).catch(()=>alert('Oops, something went wrong')).finally(setIsLoading(false))
-  },[query,page])
+  useEffect(() => {
+    if (!query) return;
+    setIsLoading('idle');
+    getImages(query, page)
+      .then(({ totalHits, hits }) => {
+        setImages(prevImgs => [...prevImgs, ...hits]);
+        setIsLoadMore(totalHits > 0 && page < Math.ceil(totalHits / 12));
+        setIsLoading('success')
+      })
+      .catch(() => {
+        setIsLoading('rejected')
+        alert('Oops, something went wrong')})
+  }, [query, page]);
 
   const handleSearch = query => {
-   setQuery(query);
-   setPage(1);
-   setImages([]);
+    setQuery(query);
+    setPage(1);
+    setImages([]);
   };
 
   const loadMore = () => {
-    setPage(prevPage=>prevPage + 1)
+    setPage(prevPage => prevPage + 1);
   };
 
   const handleModalOpen = modalImg => {
     setModalImg(modalImg);
-    setModalShow(true)
+    setModalShow(true);
   };
 
   const handleModalClose = () => {
-    setModalShow(false)
+    setModalShow(false);
   };
 
-    return (
-      <DivElem>
-        <SearchBar onSubmit={handleSearch} />
-        {isLoading && <Loader />}
-        {(images.length === 0 && query) && (
-          <p>Sorry, we didn't found pictures for this query</p>
-        )}
-        <ImageGallery images={images} modalOpen={handleModalOpen} />
-        {isLoadMore && <Button onLoadMore={loadMore} />}
-        {isModalShow && (
-          <Modal onModalClose={handleModalClose} image={modalImg} />
-        )}
-      </DivElem>
-    );
-  }
+  return (
+    <DivElem>
+      <SearchBar onSubmit={handleSearch} />
+      {isLoading === 'idle' && <Loader />}
+      {images.length === 0 && query && (
+        <p>Sorry, we didn't found pictures for this query</p>
+      )}
+      <ImageGallery images={images} modalOpen={handleModalOpen} />
+      {isLoadMore && <Button onLoadMore={loadMore} />}
+      {isModalShow && (
+        <Modal onModalClose={handleModalClose} image={modalImg} />
+      )}
+    </DivElem>
+  );
+};
